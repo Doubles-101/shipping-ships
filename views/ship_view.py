44f16822acca05de3d2e5db423b3d58a54999590
William Woodard <williamwoodard98@gmail.com>
2024-05-24 11:52:38 -0500

    added ability to expand haulers from ships

--- views/ship_view.py
+++ views/ship_view.py
@@ -77,25 +77,65 @@ def list_ships():
 
     return serialized_ships
 
-def retrieve_ship(pk):
+def retrieve_ship(pk, url_dict):
     # Open a connection to the database
     with sqlite3.connect("./shipping.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
-        # Write the SQL query to get the information you want
-        db_cursor.execute("""
-        SELECT
-            s.id,
-            s.name,
-            s.hauler_id
-        FROM Ship s
-        WHERE s.id = ?
-        """, (pk,))
-        query_results = db_cursor.fetchone()
 
-        # Serialize Python list to JSON encoded string
-        dictionary_version_of_object = dict(query_results)
-        serialized_ship = json.dumps(dictionary_version_of_object)
+        if url_dict["query_params"] == {}:
+            # Write the SQL query to get the information you want
+            db_cursor.execute("""
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id
+            FROM Ship s
+            WHERE s.id = ?
+            """, (pk,))
+            query_results = db_cursor.fetchone()
+
+            # Serialize Python list to JSON encoded string
+            dictionary_version_of_object = dict(query_results)
+            serialized_ship = json.dumps(dictionary_version_of_object)
+
+        elif "_expand" in url_dict["query_params"]:
+
+            db_cursor.execute("""
+            SELECT
+                s.id,
+                s.name,
+                s.hauler_id,
+                h.id haulerId,
+                h.name haulerName,
+                h.dock_id
+            FROM Ship s
+            JOIN Hauler h
+                ON h.id = s.hauler_id
+            WHERE s.id = ?
+            """, (pk,))
+
+            query_results = db_cursor.fetchall()
+
+            # Create the nested structure for the ship
+            ships = []
+            for row in query_results:
+                hauler = {
+                    "id": row['haulerId'],
+                    "name": row['haulerName'],
+                    "dock_id": row["dock_id"]
+                }
+                ship = {
+                    "id": row['id'],
+                    "name": row['name'],
+                    "hauler_id": row["hauler_id"],
+                    "hauler": hauler
+                }
+                ships.append(ship)
+
+            ships = json.dumps(ships)
+            return ships
+
 
     return serialized_ship
